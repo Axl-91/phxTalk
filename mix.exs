95bc77d548e19820af503c2017b0f1c6d60a150d
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2026-02-16 16:22:30 -0300

    Fix all failing tests

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule PhxChatRoom.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:lazy_html, ">= 0.1.0", only: :test}
     ]
   end
 
--- test/phx_chat_room_web/controllers/page_controller_test.exs
+++ test/phx_chat_room_web/controllers/page_controller_test.exs
@@ -3,6 +3,8 @@ defmodule PhxChatRoomWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+
+    # We'll just check that it is redirected to chat_room
+    assert redirected_to(conn) == "/chat_room"
   end
 end
