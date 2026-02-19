bba1331ab397c5c62842a0236baeee35fc9250f5
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2026-02-19 15:25:14 -0300

    Refactor of the chatroom table

--- lib/phx_chat_room_web/components/core_components.ex
+++ lib/phx_chat_room_web/components/core_components.ex
@@ -379,8 +379,9 @@ defmodule PhxChatRoomWeb.CoreComponents do
         id={@id}
         value={Phoenix.HTML.Form.normalize_value(@type, @value)}
         class={[
-          "mt-2 block w-full #{if chatroom_input?, do: "rounded-b-lg border-t-0", else: "rounded-lg"} text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
-          "border-zinc-500",
+          "mt-2 block w-full ",
+          "#{if chatroom_input?, do: "rounded-b-lg border-t-0", else: "rounded-lg"}",
+          "text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6 border-zinc-500",
           @errors == [] && "border-zinc-300 focus:border-zinc-400",
           @errors != [] && "border-rose-400 focus:border-rose-400"
         ]}
